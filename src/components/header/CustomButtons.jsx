7c58c93601f306a0a7b3d067895484c346d49965
Muhammad Ashraf <muhammadashraf2921323@gmail.com>
2024-08-27 15:08:41 -0700

    working

--- src/components/header/CustomButtons.jsx
+++ src/components/header/CustomButtons.jsx
@@ -1,17 +1,40 @@
-import { Box ,Button, Typography} from '@mui/material'
-import React from 'react'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-export default function CustomButtons() {
-  return (
-    <Box style={{display:'flex'}}>
-       
-  <Button variant='contained'>Login </Button>
-  <Typography>Become a Seller</Typography>
-  <Typography>More</Typography>
+import { Box, Button, Typography, styled } from "@mui/material";
+import React from "react";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import StorefrontIcon from "@mui/icons-material/Storefront";
 
-  <Box>
-<ShoppingCartIcon/>
-  </Box>
-    </Box>
-  )
+export default function CustomButtons() {
+  const Wrapper = styled(Box)`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `;
+
+  const ItemBox = styled(Box)`
+    display: flex;
+    align-items: center;
+    margin-left: 16px; /* Adjust spacing between items */
+  `;
+
+  return (
+    <Wrapper>
+      <ItemBox>
+        <AccountCircleOutlinedIcon />
+        <Button variant="text" color="secondary">
+          Login
+        </Button>
+      </ItemBox>
+
+      <ItemBox>
+        <ShoppingCartIcon />
+        <Typography>Cart</Typography>
+      </ItemBox>
+
+      <ItemBox>
+        <StorefrontIcon />
+        <Typography style={{ width: 135 }}>Become a Seller</Typography>
+      </ItemBox>
+    </Wrapper>
+  );
 }
